be5893d7702518336b1d97277b107f4fada2c892
i.gorbunov <i.gorbunov@space307.com>
2019-04-07 23:17:39 +0300

    remove dead code

--- src/markdown-combine.js
+++ src/markdown-combine.js
@@ -1,9 +1,7 @@
 const fs = require("fs");
 const util = require("util");
 const path = require("path");
-const { uniq } = require("lodash");
 const logger = require("./logger.js");
-const cp = require("copyfiles");
 const beautifyImages = require("./beautify-image-paths.js");
 
 const [readFile, writeFile, exists] = [fs.readFile, fs.writeFile, fs.exists].map(fn =>
@@ -12,19 +10,6 @@ const [readFile, writeFile, exists] = [fs.readFile, fs.writeFile, fs.exists].map
 
 const combineMarkdowns = ({ contents, pathToStatic, mainMdFilename }) => async links => {
   try {
-    const images = uniq(links.map(link => path.dirname(link)))
-      .map(dir => path.relative(process.cwd(), dir))
-      .map(dir => [`${dir}/**/*.jpg`, `${dir}/**/*.png`, `${dir}/**/*.gif`])
-      .reduce((acc, item) => [...acc, ...item], []);
-
-    const a = [...images, pathToStatic];
-
-    await cp(a, (...args) => {
-      const c = args;
-
-      const b = a;
-    });
-
     const files = await Promise.all(
       await links.map(async filename => {
         const fileExist = await exists(filename);
